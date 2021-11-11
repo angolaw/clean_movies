97919b706db7f72630fe26d393c5f9a6a73bd7b2
Willian S <wsapcf@gmail.com>
2021-11-11 06:02:03 -0300

    change MovieRepository base class signature

--- lib/domain/repositories/movie_repository.dart
+++ lib/domain/repositories/movie_repository.dart
@@ -1,5 +1,7 @@
+import 'package:clean_movies/domain/entities/app_error.dart';
 import 'package:clean_movies/domain/entities/movie_entity.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class MovieRepository {
-  Future<List<MovieEntity?>> getTrending();
+  Future<Either<AppError, List<MovieEntity?>>> getTrending();
 }

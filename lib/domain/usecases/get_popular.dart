d9596619dd0c9e8175a0c252dff804a4be9bf724
Willian S <wsapcf@gmail.com>
2021-11-11 06:16:38 -0300

    add getPopular

---
+++ lib/domain/usecases/get_popular.dart
@@ -0,0 +1,13 @@
+import 'package:clean_movies/domain/entities/app_error.dart';
+import 'package:clean_movies/domain/entities/movie_entity.dart';
+import 'package:clean_movies/domain/repositories/movie_repository.dart';
+import 'package:dartz/dartz.dart';
+
+class GetPopular {
+  final MovieRepository repository;
+
+  GetPopular(this.repository);
+  Future<Either<AppError, List<MovieEntity?>>> call() async {
+    return await repository.getComingSoon();
+  }
+}

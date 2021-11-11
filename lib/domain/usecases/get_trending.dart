a219bcca4d9eb0e17478322e8c1ef6de3a74f4c4
Willian S <wsapcf@gmail.com>
2021-11-11 05:54:58 -0300

    add GetTrending usecase

---
+++ lib/domain/usecases/get_trending.dart
@@ -0,0 +1,10 @@
+import 'package:clean_movies/domain/entities/movie_entity.dart';
+import 'package:clean_movies/domain/repositories/movie_repository.dart';
+
+class GetTrending {
+  final MovieRepository repository;
+  GetTrending(this.repository);
+  Future<List<MovieEntity?>> call() async {
+    return await repository.getTrending();
+  }
+}

52c2bcd2f49fbedd77c3a1ef25191648d1337505
Willian S <wsapcf@gmail.com>
2021-11-11 06:33:45 -0300

    add GetTrending usecase update

--- lib/domain/usecases/get_trending.dart
+++ lib/domain/usecases/get_trending.dart
@@ -3,10 +3,15 @@ import 'package:clean_movies/domain/entities/movie_entity.dart';
 import 'package:clean_movies/domain/repositories/movie_repository.dart';
 import 'package:dartz/dartz.dart';
 
-class GetTrending {
+import '../usecase.dart';
+import 'no_params.dart';
+
+class GetTrending extends UseCase<List<MovieEntity?>, NoParams> {
   final MovieRepository repository;
   GetTrending(this.repository);
-  Future<Either<AppError, List<MovieEntity?>>> call() async {
+
+  @override
+  Future<Either<AppError, List<MovieEntity?>>> call(NoParams params) async {
     return await repository.getTrending();
   }
 }

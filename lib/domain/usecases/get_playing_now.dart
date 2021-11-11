2db51eb2aabd7dd3dd23f6edf09c39761b5cbe66
Willian S <wsapcf@gmail.com>
2021-11-11 06:16:02 -0300

    add getPlayingNow

---
+++ lib/domain/usecases/get_playing_now.dart
@@ -0,0 +1,13 @@
+import 'package:clean_movies/domain/entities/app_error.dart';
+import 'package:clean_movies/domain/entities/movie_entity.dart';
+import 'package:clean_movies/domain/repositories/movie_repository.dart';
+import 'package:dartz/dartz.dart';
+
+class GetPlayingNow {
+  final MovieRepository repository;
+
+  GetPlayingNow(this.repository);
+  Future<Either<AppError, List<MovieEntity?>>> call() async {
+    return await repository.getPlayingNow();
+  }
+}

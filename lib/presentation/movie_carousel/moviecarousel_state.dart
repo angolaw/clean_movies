f11c6d88dc8a6807e120ae3dc67e42a6dbdcefab
Willian S <wsapcf@gmail.com>
2021-11-15 20:49:20 -0300

    update MovieCarouselState

--- lib/presentation/movie_carousel/moviecarousel_state.dart
+++ lib/presentation/movie_carousel/moviecarousel_state.dart
@@ -2,9 +2,22 @@ part of 'moviecarousel_bloc.dart';
 
 abstract class MoviecarouselState extends Equatable {
   const MoviecarouselState();
-  
+
   @override
   List<Object> get props => [];
 }
 
 class MoviecarouselInitial extends MoviecarouselState {}
+
+class MovieCarouselError extends MoviecarouselState {}
+
+class MovieCarouselLoaded extends MoviecarouselState {
+  final List<MovieEntity> movies;
+  final int defaultIndex;
+
+  MovieCarouselLoaded({required this.movies, required this.defaultIndex});
+
+  @override
+  // TODO: implement props
+  List<Object> get props => [movies, defaultIndex];
+}

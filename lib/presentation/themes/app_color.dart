c50ea652a165ae12c2c65770084c1546eb857301
Willian S <wsapcf@gmail.com>
2021-11-15 20:28:22 -0300

    add AppColor class

---
+++ lib/presentation/themes/app_color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/widgets.dart';
+
+class AppColor {
+  const AppColor._();
+  static const Color vulcan = Color(0xff141221);
+  static const Color royalBlue = Color(0xff604fef);
+  static const Color white = Color(0xffa74dbc);
+}

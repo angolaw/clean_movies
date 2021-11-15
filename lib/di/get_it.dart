cbfee73ab62ce8628664a151b27d8f5d34e3efa8
Willian S <wsapcf@gmail.com>
2021-11-15 12:45:22 -0300

    add init method to getIt class

--- lib/di/get_it.dart
+++ lib/di/get_it.dart
@@ -1,3 +1,12 @@
+import 'package:clean_movies/data/core/api_client.dart';
 import 'package:get_it/get_it.dart';
+import 'package:http/http.dart';
 
-final getItInstance = GetIt.instance;
+final getItInstance = GetIt.I;
+
+void init() {
+  //first dependency - httpClient to ApiClient
+  getItInstance.registerLazySingleton<Client>(() => Client());
+  getItInstance
+      .registerLazySingleton<ApiClient>(() => ApiClient(getItInstance()));
+}

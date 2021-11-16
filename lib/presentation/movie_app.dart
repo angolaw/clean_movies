a2233f411926cf7988a6315ad37d55bb676afec2
Willian S <wsapcf@gmail.com>
2021-11-15 21:12:33 -0300

    add ScreenUtil.init constraints

--- lib/presentation/movie_app.dart
+++ lib/presentation/movie_app.dart
@@ -1,12 +1,18 @@
+import 'dart:html';
+
+import 'package:clean_movies/common/screenutil/screen_util.dart';
 import 'package:clean_movies/presentation/themes/app_color.dart';
 import 'package:clean_movies/presentation/themes/theme_text.dart';
 import 'package:flutter/material.dart';
+
+import 'journeys/home/home_screen.dart';
 
 class MovieApp extends StatelessWidget {
   const MovieApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    ScreenUtil.init(width: 414, height: 896);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Movie App',

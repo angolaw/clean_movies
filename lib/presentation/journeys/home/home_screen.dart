5955d3d440eb06743d0333c222cadf414008c987
Willian S <wsapcf@gmail.com>
2021-11-15 21:47:37 -0300

    add diferentiation

--- lib/presentation/journeys/home/home_screen.dart
+++ lib/presentation/journeys/home/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:clean_movies/di/get_it.dart';
 import 'package:clean_movies/presentation/blocs/movie_carousel/moviecarousel_bloc.dart';
+import 'package:clean_movies/presentation/journeys/movie_carousel/movie_carousel_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,44 +12,53 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  MoviecarouselBloc? moviecarouselBloc;
+  MoviecarouselBloc? movieCarouselBloc;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    moviecarouselBloc = getItInstance<MoviecarouselBloc>();
-    moviecarouselBloc?.add(CarouselLoadEvent(defaultIndex: 0));
+    movieCarouselBloc = getItInstance<MoviecarouselBloc>();
+    movieCarouselBloc?.add(CarouselLoadEvent(defaultIndex: 0));
   }
 
   @override
   void dispose() {
     // TODO: implement dispose
     super.dispose();
-    moviecarouselBloc?.close();
+    movieCarouselBloc?.close();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        children: const [
-          FractionallySizedBox(
-            alignment: Alignment.topCenter,
-            heightFactor: 0.6,
-            child: Placeholder(
-              color: Colors.grey,
-            ),
-          ),
-          FractionallySizedBox(
-            alignment: Alignment.bottomCenter,
-            heightFactor: 0.4,
-            child: Placeholder(
-              color: Colors.white,
-            ),
-          ),
-        ],
+    return BlocProvider<MoviecarouselBloc>(
+      create: (_) => movieCarouselBloc!,
+      child: Scaffold(
+        body: BlocBuilder<MoviecarouselBloc, MoviecarouselState>(
+          bloc: movieCarouselBloc!,
+          builder: (context, state) {
+            if (state is MovieCarouselLoaded) {
+              return Stack(
+                fit: StackFit.expand,
+                children: [
+                  FractionallySizedBox(
+                      alignment: Alignment.topCenter,
+                      heightFactor: 0.6,
+                      child: MovieCarouselWidget(
+                          movies: state.movies,
+                          defaultIndex: state.defaultIndex)),
+                  const FractionallySizedBox(
+                    alignment: Alignment.bottomCenter,
+                    heightFactor: 0.4,
+                    child: Placeholder(
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              );
+            }
+          },
+        ),
       ),
     );
   }
---
+++ lib/presentation/journeys/movie_carousel/movie_carousel_widget.dart
@@ -0,0 +1,22 @@
+import 'package:clean_movies/domain/entities/movie_entity.dart';
+import 'package:clean_movies/presentation/movie_app.dart';
+import 'package:clean_movies/presentation/widgets/movie_app_bar.dart';
+import 'package:flutter/material.dart';
+
+class MovieCarouselWidget extends StatelessWidget {
+  final List<MovieEntity?> movies;
+  final int defaultIndex;
+  const MovieCarouselWidget(
+      {Key? key, required this.movies, required this.defaultIndex})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        MovieAppBar(),
+        MoviePageView(),
+      ],
+    );
+  }
+}

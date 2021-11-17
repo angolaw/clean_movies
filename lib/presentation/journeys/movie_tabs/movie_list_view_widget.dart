a380b38ef0e76f7822e2000dc7739014bab8f98a
Willian S <wsapcf@gmail.com>
2021-11-17 16:07:35 -0300

    Add movieListViewWidget

---
+++ lib/presentation/journeys/movie_tabs/movie_list_view_widget.dart
@@ -0,0 +1,34 @@
+import 'package:clean_movies/common/extensions/size_extension.dart';
+import 'package:clean_movies/domain/entities/movie_entity.dart';
+import 'package:clean_movies/presentation/journeys/movie_tabs/movie_tab_card_widget.dart';
+import 'package:flutter/material.dart';
+
+class MovieListViewBuilder extends StatelessWidget {
+  final List<MovieEntity?> movies;
+
+  const MovieListViewBuilder({Key? key, required this.movies})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 6.h.toDouble()),
+      child: ListView.separated(
+          scrollDirection: Axis.horizontal,
+          shrinkWrap: true,
+          itemBuilder: (context, index) {
+            final MovieEntity movie = movies[index]!;
+            return MovieTabCardWidget(
+                movieId: movie.id,
+                title: movie.title,
+                posterPath: movie.posterPath);
+          },
+          separatorBuilder: (context, index) {
+            return SizedBox(
+              width: 14.w.toDouble(),
+            );
+          },
+          itemCount: movies.length),
+    );
+  }
+}

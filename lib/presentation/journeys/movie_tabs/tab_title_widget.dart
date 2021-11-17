241c46b467ccda25ea3c403c593b642b6eebf10c
Willian S <wsapcf@gmail.com>
2021-11-17 15:58:36 -0300

    add TabTitleWidget

---
+++ lib/presentation/journeys/movie_tabs/tab_title_widget.dart
@@ -0,0 +1,41 @@
+import 'package:clean_movies/common/constants/size_constants.dart';
+import 'package:clean_movies/common/extensions/size_extension.dart';
+import 'package:clean_movies/common/extensions/text_theme_extension.dart';
+import 'package:clean_movies/presentation/themes/app_color.dart';
+import 'package:flutter/material.dart';
+
+class TabTitleWidget extends StatelessWidget {
+  final String title;
+  final VoidCallback onTap;
+  final bool isSelected;
+  const TabTitleWidget(
+      {Key? key,
+      required this.title,
+      required this.onTap,
+      required this.isSelected})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.transparent,
+          border: Border(
+            bottom: BorderSide(
+              color: isSelected ? AppColor.royalBlue : Colors.transparent,
+              width: Sizes.dimen_1.h.toDouble(),
+            ),
+          ),
+        ),
+        child: Text(
+          title,
+          style: isSelected
+              ? Theme.of(context).textTheme.royalBlueSubtitle1
+              : Theme.of(context).textTheme.subtitle1,
+        ),
+      ),
+    );
+  }
+}

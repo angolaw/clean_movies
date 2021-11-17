63d288a9842fbeaf4d8befe8b8a4112a669f9c54
Willian S <wsapcf@gmail.com>
2021-11-17 15:19:43 -0300

    add MovieTabChangedEvent

--- lib/presentation/blocs/movie_tab/movietab_event.dart
+++ lib/presentation/blocs/movie_tab/movietab_event.dart
@@ -1,8 +1,17 @@
 part of 'movietab_bloc.dart';
 
-abstract class MovietabEvent extends Equatable {
-  const MovietabEvent();
+abstract class MovieTabEvent extends Equatable {
+  const MovieTabEvent();
 
   @override
   List<Object> get props => [];
+}
+
+class MovieTabChangedEvent extends MovieTabEvent {
+  final int currentTabIndex;
+
+  const MovieTabChangedEvent({required this.currentTabIndex});
+
+  @override
+  List<Object> get props => [currentTabIndex];
 }

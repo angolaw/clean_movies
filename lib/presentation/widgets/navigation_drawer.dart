a6bee4f1288a041951b67ca7b9258c8d909fc39e
Willian S <wsapcf@gmail.com>
2021-11-19 14:43:14 -0300

    add dimen_300 dimension to size

---
+++ lib/presentation/widgets/navigation_drawer.dart
@@ -0,0 +1,12 @@
+import 'package:clean_movies/common/constants/size_constants.dart';
+import 'package:clean_movies/common/extensions/size_extension.dart';
+import 'package:flutter/material.dart';
+
+class NavigationDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: Sizes.dimen_300.w.toDouble(),
+    );
+  }
+}

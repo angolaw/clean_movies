71004bc4c217b5ca9938d1f8b773e4322565691a
Willian S <wsapcf@gmail.com>
2021-11-23 14:50:47 -0300

    add LanguageLoaded state

--- lib/presentation/blocs/bloc/language_state.dart
+++ lib/presentation/blocs/bloc/language_state.dart
@@ -2,9 +2,17 @@ part of 'language_bloc.dart';
 
 abstract class LanguageState extends Equatable {
   const LanguageState();
-  
+
   @override
   List<Object> get props => [];
 }
 
 class LanguageInitial extends LanguageState {}
+
+class LanguageLoaded extends LanguageState {
+  final Locale locale;
+
+  const LanguageLoaded({required this.locale});
+  @override
+  List<Object> get props => [locale.languageCode];
+}

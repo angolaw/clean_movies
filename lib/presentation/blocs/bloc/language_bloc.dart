c6fd587e9b404346415507529515cc9795e66c4f
Willian S <wsapcf@gmail.com>
2021-11-23 14:52:12 -0300

    add LanguageBloc implementation

--- lib/presentation/blocs/bloc/language_bloc.dart
+++ lib/presentation/blocs/bloc/language_bloc.dart
@@ -1,13 +1,19 @@
 import 'package:bloc/bloc.dart';
+import 'package:clean_movies/common/constants/languages.dart';
+import 'package:clean_movies/domain/entities/language_entity.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 
 part 'language_event.dart';
 part 'language_state.dart';
 
 class LanguageBloc extends Bloc<LanguageEvent, LanguageState> {
-  LanguageBloc() : super(LanguageInitial()) {
+  LanguageBloc()
+      : super(LanguageLoaded(locale: Locale(Languages.languages[0].code))) {
     on<LanguageEvent>((event, emit) {
-      // TODO: implement event handler
+      if (event is ToggleLanguageEvent) {
+        emit(LanguageLoaded(locale: Locale(event.language.code)));
+      }
     });
   }
 }

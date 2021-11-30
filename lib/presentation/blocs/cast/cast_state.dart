16c2b2ae7c496c80828ee9a7a3ef4f9e4105302b
Willian S <wsapcf@gmail.com>
2021-11-30 11:46:06 -0300

    add cast bloc state

--- lib/presentation/blocs/cast/cast_state.dart
+++ lib/presentation/blocs/cast/cast_state.dart
@@ -2,9 +2,20 @@ part of 'cast_bloc.dart';
 
 abstract class CastState extends Equatable {
   const CastState();
-  
+
   @override
   List<Object> get props => [];
 }
 
 class CastInitial extends CastState {}
+
+class CastError extends CastState {}
+
+class CastLoaded extends CastState {
+  final List<CastEntity?> cast;
+
+  CastLoaded({required this.cast});
+
+  @override
+  List<Object> get props => [cast];
+}

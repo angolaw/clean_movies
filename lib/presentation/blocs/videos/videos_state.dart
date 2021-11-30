1b15932bd77e319e9f72a5ef71fb25615ea3ab28
Willian S <wsapcf@gmail.com>
2021-11-30 13:29:45 -0300

    add VideosLoaded state

--- lib/presentation/blocs/videos/videos_state.dart
+++ lib/presentation/blocs/videos/videos_state.dart
@@ -2,9 +2,19 @@ part of 'videos_bloc.dart';
 
 abstract class VideosState extends Equatable {
   const VideosState();
-  
+
   @override
   List<Object> get props => [];
 }
 
 class VideosInitial extends VideosState {}
+
+class NoVideos extends VideosState {}
+
+class VideosLoaded extends VideosState {
+  final List<MovieEntity?> movies;
+  const VideosLoaded({required this.movies});
+
+  @override
+  List<Object> get props => [movies];
+}

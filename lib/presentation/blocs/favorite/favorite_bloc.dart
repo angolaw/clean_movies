d45ebdde85cf676889f6b61c56014b564e95debe
Willian S <wsapcf@gmail.com>
2021-12-03 22:05:28 -0300

    add usecases dependencies on bloc

--- lib/presentation/blocs/favorite/favorite_bloc.dart
+++ lib/presentation/blocs/favorite/favorite_bloc.dart
@@ -1,11 +1,26 @@
 import 'package:bloc/bloc.dart';
+import 'package:clean_movies/domain/entities/movie_entity.dart';
+import 'package:clean_movies/domain/usecases/check_if_favorite_movie.dart';
+import 'package:clean_movies/domain/usecases/delete_favorite_movie.dart';
+import 'package:clean_movies/domain/usecases/get_favorite_movies.dart';
+import 'package:clean_movies/domain/usecases/save_movie.dart';
 import 'package:equatable/equatable.dart';
 
 part 'favorite_event.dart';
 part 'favorite_state.dart';
 
 class FavoriteBloc extends Bloc<FavoriteEvent, FavoriteState> {
-  FavoriteBloc() : super(FavoriteInitial()) {
+  final SaveMovie saveMovie;
+  final GetFavoriteMovies getFavoriteMovies;
+  final DeleteFavoriteMovie deleteFavoriteMovie;
+  final CheckIfFavoriteMovie checkIfMovieIsFavorite;
+
+  FavoriteBloc({
+    required this.saveMovie,
+    required this.getFavoriteMovies,
+    required this.deleteFavoriteMovie,
+    required this.checkIfMovieIsFavorite,
+  }) : super(FavoriteInitial()) {
     on<FavoriteEvent>((event, emit) {
       // TODO: implement event handler
     });

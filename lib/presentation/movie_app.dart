d69d94212303f3d0f6fc891063c421a0c3f2d6bd
Willian S <wsapcf@gmail.com>
2021-12-06 14:32:45 -0300

    add routes call to movie_app.dart

--- lib/presentation/movie_app.dart
+++ lib/presentation/movie_app.dart
@@ -1,4 +1,5 @@
 import 'package:clean_movies/common/constants/languages.dart';
+import 'package:clean_movies/common/route_list.dart';
 import 'package:clean_movies/common/screenutil/screen_util.dart';
 import 'package:clean_movies/di/get_it.dart';
 import 'package:clean_movies/domain/app_localizations.dart';
@@ -59,6 +60,10 @@ class _MovieAppState extends State<MovieApp> {
                   GlobalWidgetsLocalizations.delegate,
                   AppLocalizations.delegate,
                 ],
+                builder: (context, child) {
+                  return child!;
+                },
+                initialRoute: RouteList.initial,
                 title: 'Movie App',
                 navigatorKey: _navigatorKey,
                 theme: ThemeData(

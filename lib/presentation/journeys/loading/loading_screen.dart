46ab4ea6cd614441626f58b8274178ccb13539c7
Willian S <wsapcf@gmail.com>
2021-12-06 20:44:03 -0300

    add widget attribute to LoadingScreen

--- lib/presentation/journeys/loading/loading_screen.dart
+++ lib/presentation/journeys/loading/loading_screen.dart
@@ -1,19 +1,34 @@
 import 'package:clean_movies/common/constants/size_constants.dart';
+import 'package:clean_movies/presentation/blocs/loading/loading_bloc.dart';
 import 'package:clean_movies/presentation/journeys/loading/loading_circle.dart';
 import 'package:clean_movies/presentation/themes/app_color.dart';
 import 'package:flutter/material.dart';
 import 'package:clean_movies/common/extensions/size_extension.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LoadingScreen extends StatelessWidget {
-  const LoadingScreen({Key? key}) : super(key: key);
+  final Widget screen;
+  const LoadingScreen({Key? key, required this.screen}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        decoration: BoxDecoration(
-          color: AppColor.vulcan.withOpacity(0.8),
-        ),
-        child:
-            Center(child: LoadingCircle(size: Sizes.dimen_200.w.toDouble())));
+    return BlocBuilder<LoadingBloc, LoadingState>(
+      builder: (context, state) {
+        return Stack(
+          fit: StackFit.expand,
+          children: [
+            screen,
+            if (state is LoadingStarted)
+              Container(
+                  decoration: BoxDecoration(
+                    color: AppColor.vulcan.withOpacity(0.8),
+                  ),
+                  child: Center(
+                      child:
+                          LoadingCircle(size: Sizes.dimen_200.w.toDouble()))),
+          ],
+        );
+      },
+    );
   }
 }

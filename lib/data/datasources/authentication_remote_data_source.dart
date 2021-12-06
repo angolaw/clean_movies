677f0abcd2c05177a9201c24e20d01f2a2f286ca
Willian S <wsapcf@gmail.com>
2021-12-06 15:28:23 -0300

    add implementation of getRequestToken

--- lib/data/datasources/authentication_remote_data_source.dart
+++ lib/data/datasources/authentication_remote_data_source.dart
@@ -1,5 +1,34 @@
+import 'package:clean_movies/data/core/api_client.dart';
+import 'package:clean_movies/data/models/request_model_token.dart';
+
 abstract class AuthenticationRemoteDataSource {
-  Future<RequestModelToken> getRequestToken();
-  Future<RequestModelToken> validateWithLogin(Map<String, dynamic> requestBody);
+  Future<RequestTokenModel> getRequestToken();
+  Future<RequestTokenModel> validateWithLogin(Map<String, dynamic> requestBody);
   Future<String> createSession(Map<String, dynamic> requestBody);
+}
+
+class AuthenticationRemoteDataSourceImpl
+    extends AuthenticationRemoteDataSource {
+  final ApiClient client;
+
+  AuthenticationRemoteDataSourceImpl({required this.client});
+  @override
+  Future<String> createSession(Map<String, dynamic> requestBody) {
+    // TODO: implement createSession
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<RequestTokenModel> getRequestToken() async {
+    final response = await client.get('authentication/token/new');
+    final requestTokenModel = RequestTokenModel.fromJson(response);
+    return requestTokenModel;
+  }
+
+  @override
+  Future<RequestTokenModel> validateWithLogin(
+      Map<String, dynamic> requestBody) {
+    // TODO: implement validateWithLogin
+    throw UnimplementedError();
+  }
 }

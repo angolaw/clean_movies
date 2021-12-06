cac03bfa061352c83c5bdaf7aaf520005158c6b4
Willian S <wsapcf@gmail.com>
2021-12-06 16:04:04 -0300

    add Login state

--- lib/presentation/blocs/login/login_state.dart
+++ lib/presentation/blocs/login/login_state.dart
@@ -2,9 +2,21 @@ part of 'login_bloc.dart';
 
 abstract class LoginState extends Equatable {
   const LoginState();
-  
+
   @override
   List<Object> get props => [];
 }
 
 class LoginInitial extends LoginState {}
+
+class LoginSuccess extends LoginState {}
+
+class LogoutSuccess extends LoginState {}
+
+class LoginError extends LoginState {
+  final String message;
+  LoginError({required this.message});
+
+  @override
+  List<Object> get props => [message];
+}

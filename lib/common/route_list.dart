b0dd29468c3fa238a85b56711dc6183918ff17ce
Willian S <wsapcf@gmail.com>
2021-12-06 14:30:17 -0300

    add route list class

---
+++ lib/common/route_list.dart
@@ -0,0 +1,8 @@
+class RouteList {
+  RouteList._();
+
+  static const String initial = '/';
+  static const String movieDetail = '/movie-detail';
+  static const String watchVideo = '/watch-video';
+  static const String favorite = '/favorite';
+}

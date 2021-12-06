e107eaefdb07b08d9d91b7d29a37232b45332b12
Willian S <wsapcf@gmail.com>
2021-12-06 11:23:12 -0300

    add concrete implementation of localLanguageDataSource

--- lib/data/datasources/language_local_data_source.dart
+++ lib/data/datasources/language_local_data_source.dart
@@ -1,4 +1,21 @@
+import 'dart:async';
+import 'package:hive/hive.dart';
+
 abstract class LanguageLocalDataSource {
   Future<void> updateLanguage(String language);
   Future<String> getPreferredLanguage();
+}
+
+class LanguageLocalDataSourceImpl extends LanguageLocalDataSource {
+  @override
+  Future<String> getPreferredLanguage() async {
+    final languageBox = Hive.box('languageBox');
+    return languageBox.get('preferredLanguage');
+  }
+
+  @override
+  Future<void> updateLanguage(String language) async {
+    final languageBox = Hive.box('languageBox');
+    unawaited(languageBox.put('preferredLanguage', language));
+  }
 }

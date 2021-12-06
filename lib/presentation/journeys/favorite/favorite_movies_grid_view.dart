1e3fe452aa68ba428df53da09de1ec88a5a9946c
Willian S <wsapcf@gmail.com>
2021-12-06 11:04:39 -0300

    add favorite movies grid view

---
+++ lib/presentation/journeys/favorite/favorite_movies_grid_view.dart
@@ -0,0 +1,13 @@
+import 'package:clean_movies/domain/entities/movie_entity.dart';
+import 'package:flutter/material.dart';
+
+class FavoriteMoviesGridView extends StatelessWidget {
+  final List<MovieEntity> movies;
+  const FavoriteMoviesGridView({Key? key, required this.movies})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

1b89737dc47357adddf21673d628228deb012f37
Willian S <wsapcf@gmail.com>
2021-12-09 09:43:31 -0300

    add color variation on logo

--- lib/presentation/widgets/logo.dart
+++ lib/presentation/widgets/logo.dart
@@ -1,5 +1,8 @@
+import 'package:clean_movies/presentation/blocs/theme/theme_cubit.dart';
+import 'package:clean_movies/presentation/themes/app_color.dart';
 import 'package:flutter/material.dart';
 import 'package:clean_movies/common/extensions/size_extension.dart';
+import 'package:provider/src/provider.dart';
 
 class Logo extends StatelessWidget {
   final double height;
@@ -7,6 +10,11 @@ class Logo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Image.asset('assets/pngs/logo.png', height: height.h.toDouble());
+    return Image.asset('assets/pngs/logo.png',
+        key: const ValueKey('logo_image_key'),
+        color: context.read<ThemeCubit>().state == Themes.dark
+            ? Colors.white
+            : AppColor.vulcan,
+        height: height.h.toDouble());
   }
 }

2fbc9c55a86c504f2f7e490ca2ea7b0530ba7e8f
Willian S <wsapcf@gmail.com>
2021-12-09 08:40:15 -0300

    add theme usecases

---
+++ lib/domain/usecases/get_preferred_theme.dart
@@ -0,0 +1,17 @@
+import 'package:clean_movies/domain/entities/app_error.dart';
+import 'package:clean_movies/domain/repositories/app_repository.dart';
+import 'package:clean_movies/domain/usecases/no_params.dart';
+import 'package:dartz/dartz.dart';
+
+import '../usecase.dart';
+
+class GetPreferredTheme extends UseCase<String, NoParams> {
+  final AppRepository repository;
+
+  GetPreferredTheme({required this.repository});
+  @override
+  Future<Either<AppError, String>> call(NoParams params) {
+    // TODO: implement call
+    throw UnimplementedError();
+  }
+}
--- lib/presentation/blocs/theme/theme_cubit.dart
+++ lib/presentation/blocs/theme/theme_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:clean_movies/domain/usecases/no_params.dart';
 import 'package:equatable/equatable.dart';
 
 part 'theme_state.dart';
@@ -9,5 +10,22 @@ enum Themes {
 }
 
 class ThemeCubit extends Cubit<ThemeState> {
-  ThemeCubit() : super(ThemeInitial());
+  final GetPreferredTheme getPreferredTheme;
+  final UpdatePreferredTheme updatePreferredTheme;
+  ThemeCubit(
+      {required this.getPreferredTheme, required this.updatePreferredTheme})
+      : super(ThemeInitial());
+
+  Future<void> toggleTheme() async {
+    await updatePreferredTheme(state == Themes.dark ? 'light' : 'dark');
+    loadPreferredTheme();
+  }
+
+  void loadPreferredTheme() async {
+    final response = await getPreferredTheme(NoParams());
+    emit(response.fold(
+      (failure) => Themes.dark,
+      (theme) => theme == 'dark' ? Themes.dark : Themes.light,
+    ));
+  }
 }

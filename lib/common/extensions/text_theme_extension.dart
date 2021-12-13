0d017c4fcb5afef9fb1fbc57f5d48a738e62ef31
Willian S <wsapcf@gmail.com>
2021-12-13 09:57:39 -0300

    remove unused imports

--- lib/common/extensions/text_theme_extension.dart
+++ lib/common/extensions/text_theme_extension.dart
@@ -1,6 +1,5 @@
 import 'package:clean_movies/presentation/themes/app_color.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 extension TextThemeExtension on TextTheme {
   TextStyle get royalBlueSubtitle1 => subtitle1!
